4580682e5f4493a818aacf6135efd4289e026298
XREC Web <web@xrecruiter.io>
2026-04-08 11:42:37 +1000

    Update

--- public/projects/prolocum/main.js
+++ public/projects/prolocum/main.js
@@ -29,6 +29,7 @@ gsap.ticker.lagSmoothing(0);
 
 document.addEventListener('DOMContentLoaded', () => {
   if (document.querySelector('[data-example]')) initExample();
+  if (document.querySelector('[data-swiper-group]')) initSwiperSlider();
 });
 
 
@@ -37,4 +38,41 @@ document.addEventListener('DOMContentLoaded', () => {
 // EXAMPLE //
 function initExample() {
   // ...
+}
+
+// SWIPER SLIDER //
+function initSwiperSlider() {
+  const swiperSliderGroups = document.querySelectorAll('[data-swiper-group]');
+
+  swiperSliderGroups.forEach((swiperGroup) => {
+    const swiperSliderWrap = swiperGroup.querySelector('[data-swiper-wrap]');
+    if (!swiperSliderWrap) return;
+
+    const prevButton = swiperGroup.querySelector('[data-swiper-prev]');
+    const nextButton = swiperGroup.querySelector('[data-swiper-next]');
+
+    const swiper = new Swiper(swiperSliderWrap, {
+      slidesPerView: 1.25,
+      speed: 600,
+      mousewheel: true,
+      grabCursor: true,
+      breakpoints: {
+        480: { slidesPerView: 1.8 },
+        992: { slidesPerView: 3.5 }
+      },
+      navigation: {
+        nextEl: nextButton,
+        prevEl: prevButton,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+    });
+  });
 }
