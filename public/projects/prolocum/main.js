5ec7ce749a831cd5a0f101ad8113cba8a6ec5f42
XREC Web <web@xrecruiter.io>
2026-04-08 14:31:58 +1000

    Update

--- public/projects/prolocum/main.js
+++ public/projects/prolocum/main.js
@@ -32,6 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
   if (document.querySelector('[data-reveal], [data-reveal-clip]')) initReveal();
   if (document.querySelector('.img:not(.no-para)')) initImageScrollEffect();
   if (document.querySelector('[data-current-year]')) initDynamicCurrentYear();
+  if (document.querySelector('[data-accordion-css-init]')) initAccordionCSS();
 });
 
 
@@ -300,5 +301,30 @@ function initDynamicCurrentYear() {
   const currentYear = new Date().getFullYear();
   document.querySelectorAll('[data-current-year]').forEach(el => {
     el.textContent = currentYear;
+  });
+}
+
+// ACCORDION //
+function initAccordionCSS() {
+  document.querySelectorAll('[data-accordion-css-init]').forEach((accordion) => {
+    const closeSiblings = accordion.getAttribute('data-accordion-close-siblings') === 'true';
+
+    accordion.addEventListener('click', (event) => {
+      const toggle = event.target.closest('[data-accordion-toggle]');
+      if (!toggle) return; // Exit if the clicked element is not a toggle
+
+      const singleAccordion = toggle.closest('[data-accordion-status]');
+      if (!singleAccordion) return; // Exit if no accordion container is found
+
+      const isActive = singleAccordion.getAttribute('data-accordion-status') === 'active';
+      singleAccordion.setAttribute('data-accordion-status', isActive ? 'not-active' : 'active');
+      
+      // When [data-accordion-close-siblings="true"]
+      if (closeSiblings && !isActive) {
+        accordion.querySelectorAll('[data-accordion-status="active"]').forEach((sibling) => {
+          if (sibling !== singleAccordion) sibling.setAttribute('data-accordion-status', 'not-active');
+        });
+      }
+    });
   });
 }
