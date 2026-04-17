10917d56fc66c7190e153b03ed894deb18d88f90
XREC Web <web@xrecruiter.io>
2026-04-17 15:41:35 +1000

    Update

--- public/projects/lime/main.js
+++ public/projects/lime/main.js
@@ -36,6 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
   if (document.querySelector('.h-process_grid')) initProcessBlockTopFade();
   if (document.querySelector('[data-popup]')) initPopupForm();
   if (document.querySelector('[data-current-year]')) initDynamicCurrentYear();
+  if (document.querySelector('[data-swiper-group]')) initSwiperSlider();
 });
 
 // ─── FUNCTIONS ───────────────────────────────────────────────────────────────
@@ -588,5 +589,49 @@ function initDynamicCurrentYear() {
   const currentYearElements = document.querySelectorAll('[data-current-year]');
   currentYearElements.forEach(currentYearElement => {
     currentYearElement.textContent = currentYear;
+  });
+}
+
+// TESTIMONIAL SWIPER CONFIG //
+function initSwiperSlider() {  
+  const swiperSliderGroups = document.querySelectorAll("[data-swiper-group]");
+  
+  swiperSliderGroups.forEach((swiperGroup) => {
+    const swiperSliderWrap = swiperGroup.querySelector("[data-swiper-wrap]");
+    if(!swiperSliderWrap) return;
+    
+    const prevButton = swiperGroup.querySelector("[data-swiper-prev]");
+    const nextButton = swiperGroup.querySelector("[data-swiper-next]");
+    
+    const swiper = new Swiper(swiperSliderWrap, {
+      slidesPerView: 1.25,
+      speed: 600,
+      mousewheel: true,
+      grabCursor: true,
+      breakpoints: {
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 1.8,
+        },
+        // when window width is >= 992px
+        992: {
+          slidesPerView: 3.5,
+        }
+      },
+      navigation: {
+        nextEl: nextButton,
+        prevEl: prevButton,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },    
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },      
+    });    
+    
   });
 }
