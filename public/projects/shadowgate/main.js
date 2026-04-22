ed592c334c5cdf53d1d33d50fa71c4c5f0747691
XREC Web <web@xrecruiter.io>
2026-04-22 11:34:38 +1000

    Update

--- public/projects/shadowgate/main.js
+++ public/projects/shadowgate/main.js
@@ -703,7 +703,7 @@ function initHeroTitleReveal() {
   gsap.timeline({
     scrollTrigger: {
       trigger: grid,
-      start: 'bottom bottom',
+      start: 'top bottom',
       toggleActions: 'play none none reverse',
     }
   })
