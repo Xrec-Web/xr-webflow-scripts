11fea46b5e3f784c1622741dc4f8ca88a408f5ea
XREC Web <web@xrecruiter.io>
2026-04-29 13:42:39 +1000

    Update

--- public/projects/shadowgate/main.js
+++ public/projects/shadowgate/main.js
@@ -48,6 +48,7 @@ function initOnceFunctions() {
 
 function initSuperform(container) {
   if (!container.querySelector('[sf-form-block]')) return;
+  document.querySelectorAll('script[src*="superform"]').forEach(s => s.remove());
   const script = document.createElement('script');
   script.src = 'https://cdn.jsdelivr.net/npm/@deltaclan/superform@2/dist/superform.js';
   document.head.appendChild(script);
